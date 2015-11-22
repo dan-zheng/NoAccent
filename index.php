f4d15bb7a91edcfe20e1c01d067582786cbf42c0
Dan Zheng <zheng321@purdue.edu>
2015-11-22 00:34:47 -0600

    Add cat img

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
     <link rel='stylesheet' href='bower_components/bootstrap/dist/css/bootstrap.min.css'>
     <link rel='stylesheet' href='bower_components/font-awesome/css/font-awesome.min.css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/header.css" type="text/stylesheet">
     <link rel="stylesheet" href="css/fonts.css" type="text/stylesheet">
     <link rel="stylesheet" href="css/whale.css" type="text/stylesheet">
@@ -29,12 +29,13 @@
 <body>
   <div class='container text-center'>
       <div class='section-module'>
+          <img id='cat' src='cat-nobg.png'></img>
           <h1 class='section-divider' id='title'>No Accent!</h1>
           <h3 class='section-divider' id='subtitle'>A foreign accent correction app.</h3>
       </div>
       <div class='section-module'>
           <div class='videoWrapper'>
-              <iframe src="https://player.vimeo.com/video/139043174" width="960px" height="540px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/139043174">Accent!</a> from <a href="https://vimeo.com/user33161213">杨英华&#039;s work I</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+              <iframe src="https://player.vimeo.com/video/139043174" width="800px" height="480px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/139043174">Accent!</a> from <a href="https://vimeo.com/user33161213">杨英华&#039;s work I</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
           </div>
       </div>
   </div>
