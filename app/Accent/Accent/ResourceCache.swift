e4a112b4aca4712b11998de8133b3ec6cd18d95e
Richard Wei <xinranmsn@gmail.com>
2015-11-22 12:01:27 -0600

    implemented recording

---
+++ app/Accent/Accent/ResourceCache.swift
@@ -0,0 +1,13 @@
+//
+//  ResourceCache.swift
+//  Accent
+//
+//  Created by Richard Wei on 11/22/15.
+//  Copyright © 2015 accent. All rights reserved.
+//
+
+import UIKit
+
+class ResourceCache: NSObject {
+
+}
