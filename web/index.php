1b85592459d91269042130a6946be70ac6695dc4
Dan Zheng <zheng321@purdue.edu>
2015-11-22 02:00:10 -0600

    Edit index css

--- web/index.php
+++ web/index.php
@@ -24,7 +24,28 @@
     <script type="text/javascript" src='js/index.js'></script>
 </head>
 
-<?php include("header.php");?>
+<nav class='navbar navbar-inverse navbar-fixed-top noselect'>
+    <div class='container'>
+        <div class='navbar-header page-scroll'>
+            <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
+                <span class='sr-only'>Toggle navigation</span>
+                <span class='icon-bar'></span>
+                <span class='icon-bar'></span>
+                <span class='icon-bar'></span>
+            </button>
+            <a class='navbar-brand page-scroll' href='index.php'>
+                <i class='icon-picker fa fa-comments-o'></i>No Accent!
+            </a>
+        </div>
+        <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+            <ul class='nav navbar-nav navbar-right'>
+                <li><a class="page-scroll" href='#page-top'>Home</a></li>
+                <li><a class="page-scroll" id='nav-about' href='#about'>About</a>
+                <li><a class="page-scroll" id='nav-contact' href='#demo'>Demo</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
 <body>
     <section id='page-top'>
@@ -40,14 +61,15 @@
     </section>
 
     <section id='demo'>
-      <div class='text-center section-module'>
-          <h2>Demo</h1>
-          <div class='videoWrapper'>
-              <iframe src="https://player.vimeo.com/video/139043174" width="800px" height="480px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/139043174">Accent!</a> from <a href="https://vimeo.com/user33161213">杨英华&#039;s work I</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-          </div>
-      </div>
+        <div class='container text-center'>
+            <div class='section-module'>
+                <h2 class='section-center section-divider'>Demo</h1>
+                <div class='wrap'>
+                    <iframe src="https://player.vimeo.com/video/139043174" width="800px" height="480px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/139043174">Accent!</a> from <a href="https://vimeo.com/user33161213">杨英华&#039;s work I</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+                </div>
+            </div>
+        </div>
     </section>
-    <br><br><br><br><br><br><br><br><br><br><br>
 </body>
 
 </html>
